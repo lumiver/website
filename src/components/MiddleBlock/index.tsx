fd3d98bf39939504a7d98be50f88d8707bb1c721
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2025-01-20 20:05:53 -0800

    Update index.tsx

--- src/components/MiddleBlock/index.tsx
+++ src/components/MiddleBlock/index.tsx
@@ -1,7 +1,6 @@
 import { Row, Col } from "antd";
 import { withTranslation, TFunction } from "react-i18next";
 import { Slide } from "react-awesome-reveal";
-import { Button } from "../../common/Button";
 import { MiddleBlockSection, Content, ContentWrapper } from "./styles";
 
 interface MiddleBlockProps {
@@ -11,12 +10,6 @@ interface MiddleBlockProps {
 }
 
 const MiddleBlock = ({ title, content, t }: MiddleBlockProps) => {
-  const scrollTo = (id: string) => {
-    const element = document.getElementById(id) as HTMLDivElement;
-    element.scrollIntoView({
-      behavior: "smooth",
-    });
-  };
   return (
     <MiddleBlockSection>
       <Slide direction="up" triggerOnce>
